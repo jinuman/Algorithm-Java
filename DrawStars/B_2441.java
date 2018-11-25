92b04878129b49f2427555e1c8a2d67f830ed91d
jinuman <jin35kim@gmail.com>
2018-11-25 19:48:21 +0900

    solve: Baekjoon 2441

---
+++ DrawStars/B_2441.java
@@ -0,0 +1,37 @@
+package DrawStars;
+// 별찍기 - 4
+// *****
+//  ****
+//   ***
+//    **
+//     *
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B_2441 {
+    private static void drawStar(int n) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                sb.append(" ");
+            }
+            for (int j = i; j < n; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb.toString());
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        drawStar(n);
+    }
+}
+
+/*
+5
+ */
