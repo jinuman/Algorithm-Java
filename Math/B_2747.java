e7aa1de79265b82eb41b5de8396765b7995ac8ea
jinuman <jin35kim@gmail.com>
2019-01-13 22:31:03 +0900

    solve: Baekjoon 2747

---
+++ Math/B_2747.java
@@ -0,0 +1,23 @@
+package Math;
+// 피보나치 수
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B_2747 {
+    private static int[] dp = new int[46];
+    private static int fibonacciNumber(int n) {
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= 45; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(fibonacciNumber(n));
+    }
+}
