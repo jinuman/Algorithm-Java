7fb1cc849a119e3fe7673df7a333be224f508480
jinuman <jin35kim@gmail.com>
2019-01-22 11:44:54 +0900

    solve: Programmers 약수의 합

---
+++ Basic/P_SumOfDivisors.java
@@ -0,0 +1,21 @@
+package Basic;
+// 약수의 합
+// for 문을 n/2만 돌리는 방법
+
+public class P_SumOfDivisors {
+    private int solution(int n) {
+        int answer = 0;
+        for (int i = 1; i <= n/2; i++) {
+            if (n % i == 0) {
+                answer += i;
+            }
+        }
+        return answer + n;
+    }
+
+    public static void main(String[] args) {
+        P_SumOfDivisors ins = new P_SumOfDivisors();
+        System.out.println(ins.solution(12));
+        System.out.println(ins.solution(5));
+    }
+}
