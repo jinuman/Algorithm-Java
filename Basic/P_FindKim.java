c5e359dcb21056832e0c5dc88a814d3b8c229168
jinuman <jin35kim@gmail.com>
2019-01-27 18:18:28 +0900

    solve: Programmers 서울에서 김서방 찾기

---
+++ Basic/P_FindKim.java
@@ -0,0 +1,19 @@
+package Basic;
+// 서울에서 김서방 찾기
+
+public class P_FindKim {
+    private String solution(String[] seoul) {
+        int idx = -1;
+        for (int i = 0; i < seoul.length; i++) {
+            if (seoul[i].equals("Kim")) {
+                idx = i;
+            }
+        }
+        return "김서방은 " + idx + "에 있다";
+    }
+    public static void main(String[] args) {
+        P_FindKim ins = new P_FindKim();
+        String[] s = {"Jane", "Kim"};
+        System.out.println(ins.solution(s));
+    }
+}
