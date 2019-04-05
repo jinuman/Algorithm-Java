6d5cbbdfca27406a86d69be5cb211699dabb693e
jinuman <jin35kim@gmail.com>
2019-04-05 18:33:10 +0900

    solve: Programmers x만큼 간격이 있는 n개의 숫자

---
+++ Basic/P_SpacedByX.java
@@ -0,0 +1,22 @@
+package Basic;
+// x만큼 간격이 있는 n개의 숫자
+
+import java.util.Arrays;
+
+public class P_SpacedByX {
+    public static long[] solution(int x, int n) {
+        long[] answer = new long[n];
+        answer[0] = x;
+        for (int i = 1; i < n; i++) {
+            answer[i] = answer[i - 1] + x;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution(0, 3)));    // [0, 0, 0]
+        System.out.println(Arrays.toString(solution(2, 5)));    // [2, 4, 6, 8, 10]
+        System.out.println(Arrays.toString(solution(4, 3)));    // [4, 8, 12]
+        System.out.println(Arrays.toString(solution(-4, 2)));   // [-4, -8]
+    }
+}
